c1283f2acb50d5e70dc802b41602d6e9b65ea3d8
JuanGR-FI <juan60430@hotmail.com>
2023-08-07 23:04:23 -0600

    Adding Localizable strings for Italian language

--- GOW/Controller/LocustWeaponsController.swift
+++ GOW/Controller/LocustWeaponsController.swift
@@ -12,30 +12,30 @@ class LocustWeaponsController: UIViewController, UITableViewDelegate, UITableVie
     
     var arrayWeapons : [Weapon] = []
 
-    //    Locus Weapons    
+    //    Locus Weapons
     let locusWeapons : [Weapon] = [
         Weapon(
-            id: 1, name: "Boomshot Grenade Launcher",
+            id: 1, name: "weapon.boomshot",
             description: "Was a single-shot, pump-action grenade launcher designed and used primarily by Locust, specifically the Boomer.",
             poster: "BoomshotGrenadeLauncher"
         ),
         Weapon(
-            id: 2, name: "Hammerburst II",
+            id: 2, name: "weapon.hammerburst",
             description: "was the Locust standard-issue, high-powered assault rifle. It is often referred to as the Locust counterpart to the COG Lancer Assault Rifle.",
             poster: "HammerburstII"
         ),
         Weapon(
-            id: 3, name: "Mulcher",
+            id: 3, name: "weapon.mulcher",
             description: "The Mulcher is a heavy, hand-cranked, rotary gatling gun developed by the Coalition of Ordered Governments.",
             poster: "Mulcher"
         ),
         Weapon(
-            id: 4, name: "Bolo Grenade",
+            id: 4, name: "weapon.bolo.grenade",
             description: "The Bolo Grenade, otherwise known as the Fragmentation Grenade, was the standard-issue fragmentation grenade used by the Coalition of Ordered Governments, the Locust Horde and the Swarm.",
             poster: "BoloGrenade"
         ),
         Weapon(
-            id: 5, name: "Boltok Pistol",
+            id: 5, name: "weapon.boltok",
             description: "was the standard-issue sidearm of the Locust, specifically the Drones. The pistol continues the Locust weapon-design philosophy of creating weapons with an emphasis towards brute force.",
             poster: "BoltokPistol"
         )
@@ -54,7 +54,7 @@ class LocustWeaponsController: UIViewController, UITableViewDelegate, UITableVie
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell3") as! WeaponCell
-        cell.wName.text = arrayWeapons[indexPath.row].name
+        cell.wName.text = NSLocalizedString(arrayWeapons[indexPath.row].name, comment: "")
         cell.wImage.image = UIImage(named: arrayWeapons[indexPath.row].poster)
         cell.wDescription.text = arrayWeapons[indexPath.row].description
         return cell
--- GOW/Controller/WeaponsController.swift
+++ GOW/Controller/WeaponsController.swift
@@ -16,28 +16,28 @@ class WeaponsController: UIViewController, UITableViewDelegate, UITableViewDataS
     //    CGO Weapons
     let cgoWeapons : [Weapon] = [
         Weapon(
-            id: 1, name: "Mark 1 Lancer Assault Rifle",
-            description: "Dubbed the Retro Lancer during the Lambent Pandemic, was the predecessor to the Mark 2 Lancer Assault Rifle. Formerly the main assault rifle of the Coalition of Ordered Governments Army, the Mark 1 Assault Rifle was used throughout the Pendulum War.",
+            id: 1, name: "weapon.lancer.mark1",
+            description: "weapon.lancer.mark1.description",
             poster: "Mark1LancerAssaultRifle"
         ),
         Weapon(
-            id: 2, name: "Mark 2 Lancer Assault Rifle",
-            description: "An upgrade to the Mark 1 Lancer Assault Rifle, produced with a chainsaw bayonet, increased accuracy, lower recoil, and larger ammunition carrying capacity but less firepower compared to its predecessor.",
+            id: 2, name: "weapon.lancer.mark2",
+            description: "weapon.lancer.mark2.description",
             poster: "LancerMk2"
         ),
         Weapon(
-            id: 3, name: "Longshot Sniper Rifle",
-            description: "Was a high-caliber, single-shot, bolt-action sniper rifle. Created by the Coalition of Ordered Governments during the Pendulum Wars, the Longshot was used even seventeen years after Emergence Day.",
+            id: 3, name: "weapon.longshot.sniper",
+            description: "weapon.longshot.sniper.description",
             poster: "LongshotSniperRifle"
         ),
         Weapon(
-            id: 4, name: "Hammer of Dawn",
-            description: "The Hammer of Dawn was a COG Imulsion-energized, orbital, satellite-based laser. Its key components were invented by the UIR scientist Mauris Ivo and completed by professor Adam Fenix after it was stolen during Operation: Leveler.",
+            id: 4, name: "weapon.hammer.dawn",
+            description: "weapon.hammer.dawn.description",
             poster: "HammerOfDawn"
         ),
         Weapon(
-            id: 5, name: "MX8 Snub Pistol",
-            description: "Was the standard sidearm for Coalition of Ordered Governments armed forces personnel",
+            id: 5, name: "weapon.snub.pistol",
+            description: "weapon.snub.pistol.description",
             poster: "SnubPistol"
         )
     ]
@@ -58,9 +58,9 @@ class WeaponsController: UIViewController, UITableViewDelegate, UITableViewDataS
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell1") as! WeaponCell
-        cell.wName.text = arrayWeapons[indexPath.row].name
+        cell.wName.text = NSLocalizedString(arrayWeapons[indexPath.row].name, comment: "")
         cell.wImage.image = UIImage(named: arrayWeapons[indexPath.row].poster)
-        cell.wDescription.text = arrayWeapons[indexPath.row].description
+        cell.wDescription.text = NSLocalizedString(arrayWeapons[indexPath.row].description, comment: "")
         return cell
     }
     
