ead072523a6c956b38ba1568cb3dec4aab2aef86
JuanGR-FI <juan60430@hotmail.com>
2023-08-09 20:03:35 -0600

    Adding Localizable.strings for Spanish

--- GOW/Controller/LocustWeaponsController.swift
+++ GOW/Controller/LocustWeaponsController.swift
@@ -16,27 +16,27 @@ class LocustWeaponsController: UIViewController, UITableViewDelegate, UITableVie
     let locusWeapons : [Weapon] = [
         Weapon(
             id: 1, name: "weapon.boomshot",
-            description: "Was a single-shot, pump-action grenade launcher designed and used primarily by Locust, specifically the Boomer.",
+            description: "weapon.boomshot.description",
             poster: "BoomshotGrenadeLauncher"
         ),
         Weapon(
             id: 2, name: "weapon.hammerburst",
-            description: "was the Locust standard-issue, high-powered assault rifle. It is often referred to as the Locust counterpart to the COG Lancer Assault Rifle.",
+            description: "weapon.hammerburst.description",
             poster: "HammerburstII"
         ),
         Weapon(
             id: 3, name: "weapon.mulcher",
-            description: "The Mulcher is a heavy, hand-cranked, rotary gatling gun developed by the Coalition of Ordered Governments.",
+            description: "weapon.mulcher.description",
             poster: "Mulcher"
         ),
         Weapon(
             id: 4, name: "weapon.bolo.grenade",
-            description: "The Bolo Grenade, otherwise known as the Fragmentation Grenade, was the standard-issue fragmentation grenade used by the Coalition of Ordered Governments, the Locust Horde and the Swarm.",
+            description: "weapon.bolo.grenade.description",
             poster: "BoloGrenade"
         ),
         Weapon(
             id: 5, name: "weapon.boltok",
-            description: "was the standard-issue sidearm of the Locust, specifically the Drones. The pistol continues the Locust weapon-design philosophy of creating weapons with an emphasis towards brute force.",
+            description: "weapon.boltok.description",
             poster: "BoltokPistol"
         )
     ]
@@ -56,7 +56,7 @@ class LocustWeaponsController: UIViewController, UITableViewDelegate, UITableVie
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell3") as! WeaponCell
         cell.wName.text = NSLocalizedString(arrayWeapons[indexPath.row].name, comment: "")
         cell.wImage.image = UIImage(named: arrayWeapons[indexPath.row].poster)
-        cell.wDescription.text = arrayWeapons[indexPath.row].description
+        cell.wDescription.text = NSLocalizedString(arrayWeapons[indexPath.row].description, comment: "")
         return cell
     }
     
